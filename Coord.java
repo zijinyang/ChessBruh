6e68b61d9be8ab531accbbc0dadd96a6a6aa0c8c
Nathan Dai <nathandai2000@gmail.com>
2022-12-05 10:00:07 -0800

    pieces

---
+++ Coord.java
@@ -0,0 +1,30 @@
+public class Coord {
+	private final int row, col;
+
+	public Coord(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		Coord coord = (Coord) o;
+		return row == coord.row && col == coord.col;
+	}
+}
--- Piece.java
+++ Piece.java
@@ -1,5 +1,14 @@
-import org.javatuples.Pair;
-
 public interface Piece {
+	private int row, col;
 
+	public void setRow(int row);
+	public void setCol(int col);
+	public int getRow();
+	public int getCol();
+
+	public Coord getCoord();
+	public void setCoord(Coord coord);
+
+	public ArrayList<Coord> possibleMoves();
+	public boolean move(int drow, int dcol);
 }
